1c7876b44c31818a827dff9ac53be08013172c46
yap-john <john.yap@opswerks.com>
2024-09-05 15:58:41 +0800

    Update eks-terraform.sh

--- eks-terraform/eks-terraform.sh
+++ eks-terraform/eks-terraform.sh
@@ -8,6 +8,7 @@ echo "Setting up worker nodes connection"
 eksClusterName=demo-eks
 instanceRole=$(grep -rnw terraform_outputs -e NodeInstanceRole | cut -d '"' -f2)
 configMapFile=aws-auth-cm.yaml
+albsvcFile=alb-svc.yaml
 
 # Config for kubectl access
 aws eks update-kubeconfig --region us-east-1 --name $eksClusterName
@@ -16,5 +17,8 @@ aws eks update-kubeconfig --region us-east-1 --name $eksClusterName
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/cloudformation/2020-10-29/$configMapFile
 sed -i "s|rolearn: .*|rolearn: $instanceRole|" $configMapFile
 kubectl apply -f $configMapFile
+
+# Setup load balancer for web-app 
+kubectl apply -f $albsvcFile
 
 echo "Script execution completed"
