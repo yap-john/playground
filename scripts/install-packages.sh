26ab36edf49728b2572632c890530cee4e9d1e6e
yap-john <john.yap@opswerks.com>
2024-09-06 16:15:55 +0800

    Update install-packages.sh

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,10 +1,12 @@
-python3 -m ensurepip --upgrade
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath
-pipx install --include-deps ansible
- 
-pip install kubernetes
+#Uses centos OS.
+#install ansible 
+sudo python3 -m ensurepip --upgrade
+sudo python3 -m pip install --user pipx
+sudo python3 -m pipx ensurepath
+sudo pipx install --include-deps ansible
+sudo pip install kubernetes
 
+#install kubectl
 sudo yum install -y curl gnupg
 sudo tee /etc/yum.repos.d/kubernetes.repo <<EOF
 [kubernetes]
@@ -17,12 +19,15 @@ EOF
 sudo yum makecache
 sudo yum install -y kubectl-1.30.1-150500.1.1
 
+#install terraform
 curl -LO https://releases.hashicorp.com/terraform/1.6.5/terraform_1.6.5_linux_amd64.zip
 unzip terraform_1.6.5_linux_amd64.zip 
 sudo mv terraform /usr/local/bin
 
+#install awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip "awscliv2.zip"
 ./aws/install
 
+#install git
 sudo yum install -y git
