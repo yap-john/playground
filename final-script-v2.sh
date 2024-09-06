ff3f3497648465836b1c253b652fc9853271699f
yap-john <john.yap@opswerks.com>
2024-09-06 19:46:11 +0800

    Update final-script-v2.sh

--- final-script-v2.sh
+++ final-script-v2.sh
@@ -11,25 +11,21 @@
 git_url='https://github.com/yap-john/playground.git'
 git_folder=$(echo $git_url | rev | cut -d "/" -f1 | rev | cut -d "." -f1)
 
-# repo
+# Variables
 repo_dir="$(pwd)/$git_folder"
-
-#resource paths
 eks_path="$repo_dir/eks-cluster"
 init_terra_path="$repo_dir/init-terraform"
 package_path="$repo_dir/packages"
 app_path="$repo_dir/web-app"
 
-#scriptfiles
 install_packages_script="$package_path/install-packages.sh"
 terraform_init="$init_terra_path/terraform.sh"
 terraform_eks="$eks_path/terraform-eks.sh"
 setup_workers="$eks_path/setup-workers.sh"
 deploy_apps="$app_path/deploy-apps.sh"
 
-#echo $git_folder
-##clone branch containing the scripts
-#git clone --branch $branch_name $git_url
+# Clone branch containing the scripts
+git clone --branch $branch_name $git_url
 
 echo "-----Initiating script run-----"
 
@@ -44,28 +40,25 @@ echo "6) Deploy webapps"
 echo "Choose a script to run (1-6)"
 read user_input
 
-echo "-----Cloning repository-----"
-git clone $git_url
-
-if [ -z "$user_input" ]; then
-          echo "No command entered. Please try again."
-            exit 1
-fi
-
-# Function to change directory and run a script
+# Function to change directory, run a script, and return
 run_script() {
     local script_name="$1"
+    local dir="$2"
+    if [ -n "$dir" ]; then
+        cd "$dir" || { echo "Failed to change directory to $dir"; exit 1; }
+    fi
     echo "Running $script_name..."
     sh "$script_name"
+    cd - || exit
 }
 
 # Function to run all scripts
 run_all_scripts() {
-    run_script "$install_packages_script"
-    run_script "$terraform_init"
-    run_script "$terraform_eks"
-    run_script "$setup_workers"
-    run_script "$deploy_apps"
+    run_script "$install_packages_script" "$package_path"
+    run_script "$terraform_init" "$init_terra_path"
+    run_script "$terraform_eks" "$eks_path"
+    run_script "$setup_workers" "$eks_path"
+    run_script "$deploy_apps" "$app_path"
 }
 
 # Main logic based on user input
@@ -76,35 +69,31 @@ case "$user_input" in
         ;;
     2)
         echo "----- Installing required packages on machine"
-        run_script "$install_packages_script"
+        run_script "$install_packages_script" "$package_path"
         ;;
     3)
         echo "----- Running terraform for infra provisioning"
-	cd $init_terra_path
-        run_script "$terraform_init"
+        run_script "$terraform_init" "$init_terra_path"
         ;;
     4)
         echo "----- Running terraform for eks cluster"
-        cd $eks_path
-	run_script "$terraform_eks"
+        run_script "$terraform_eks" "$eks_path"
         ;;
     5)
         echo "----- Setting up connection for the worker nodes"
-        run_script "$setup_workers"
+        run_script "$setup_workers" "$eks_path"
         ;;
     6)
         echo "----- Deploying web applications"
-        run_script "$deploy_apps"
+        run_script "$deploy_apps" "$app_path"
         ;;
     *)
         echo "----- Invalid user input"
         ;;
 esac
 
-#to run scripts, relative path to the sh file and run.
-#e.g. ./$git_folder/scripts/<sh file>
-
+# Clean up cloned files
 echo "-----Cleaning up cloned files-----"
-#rm -rf $git_folder
+rm -rf "$repo_dir"
 
 echo "-----Script execution completed-----"
